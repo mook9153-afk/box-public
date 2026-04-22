9bdc1c1a48fbddd8f2fbbd91a3004dde63be0ac8
김영목 <mook9153@naver.com>
2026-04-22 12:27:55 +0900

    문의 완료 페이지에서 전화 문의 박스 제거

--- src/pages/ThankYouPage.jsx
+++ src/pages/ThankYouPage.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom'
 import Layout from '../components/Layout.jsx'
-import { CheckIcon, PhoneIcon } from '../components/Icons.jsx'
+import { CheckIcon } from '../components/Icons.jsx'
 
 function ThankYouPage() {
   const navigate = useNavigate()
@@ -18,18 +18,6 @@ function ThankYouPage() {
           <p className="thank-you-sub">
             영업일 기준 24시간 이내에 담당자가 연락드리겠습니다.
           </p>
-
-          <div className="contact-card">
-            <div className="contact-row">
-              <span className="contact-icon">
-                <PhoneIcon size={18} />
-              </span>
-              <div className="contact-body">
-                <span className="contact-label">전화 및 문자 문의</span>
-                <a href="tel:010-4733-7440">010-4733-7440</a>
-              </div>
-            </div>
-          </div>
 
           <button
             type="button"
